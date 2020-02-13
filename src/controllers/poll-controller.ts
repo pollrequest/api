18358572333ebe2964dbf98be6f4897d5b321797
rocknologie <rocknologie@hotmail.fr>
2020-02-13 17:09:42 +0100

    Add endpoints for comments

--- src/controllers/poll-controller.ts
+++ src/controllers/poll-controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from 'express';
 import ServiceContainer from '../services/service-container';
-import { AccessTokenData, RefreshTokenData } from '../services/token-service';
 import Controller from './controller';
-import { PollInstance } from '../models/poll-model';
-import { UserInstance } from '../models/user-model';
+import _ from 'lodash';
 
 /**
  * Users controller.
@@ -17,18 +15,38 @@ export default class PollController extends Controller {
      */
     public constructor(container: ServiceContainer) {
         super(container, '/polls');
+
+        // Polls
         this.addPollHandler = this.addPollHandler.bind(this);
         this.deletePollHandler = this.deletePollHandler.bind(this);
         this.getPollHandler = this.getPollHandler.bind(this);
         this.listPollsHandler = this.listPollsHandler.bind(this);
         this.modifyPollHandler = this.modifyPollHandler.bind(this);
         this.updatePollHandler = this.updatePollHandler.bind(this);
+
+        // Comments
+        this.addCommentPollHandler = this.addCommentPollHandler.bind(this);
+        this.deleteCommentPollHandler = this.deleteCommentPollHandler.bind(this);
+        this.getCommentPollHandler = this.getCommentPollHandler.bind(this);
+        this.listCommentsPollHandler = this.listCommentsPollHandler.bind(this);
+        this.modifyCommentPollHandler = this.modifyCommentPollHandler.bind(this);
+        this.updateCommentPollHandler = this.updateCommentPollHandler.bind(this);
+
+        // Polls
         this.registerEndpoint({ method: 'POST', uri: '/', handlers: [this.container.auth.authenticateHandler, this.addPollHandler], description: 'Add a poll' });
         this.registerEndpoint({ method: 'DELETE', uri: '/:id', handlers: [this.deletePollHandler], description: 'Deletes a poll' });
         this.registerEndpoint({ method: 'GET', uri: '/:id', handlers: [this.getPollHandler], description: 'Get a poll' });
         this.registerEndpoint({ method: 'GET', uri: '/', handlers: [this.listPollsHandler], description: 'List all polls' });
         this.registerEndpoint({ method: 'PUT', uri: '/:id', handlers: [this.modifyPollHandler], description: 'Modify a poll' });
         this.registerEndpoint({ method: 'PATCH', uri: '/:id', handlers: [this.updatePollHandler], description: 'Update a poll' });
+
+        // Comments
+        this.registerEndpoint({ method: 'POST', uri: '/:id/comments', handlers: [this.container.auth.authenticateHandler, this.addCommentPollHandler], description: 'Add a comment to a poll' });
+        this.registerEndpoint({ method: 'DELETE', uri: '/:pollId/comments/:commentId', handlers: [this.deleteCommentPollHandler], description: 'Delete a comment' });
+        this.registerEndpoint({ method: 'GET', uri: '/:pollId/comments/:commentId', handlers: [this.getCommentPollHandler], description: 'Get a comment' });
+        this.registerEndpoint({ method: 'GET', uri: '/:id/comments', handlers: [this.listCommentsPollHandler], description: 'List all comments' });
+        this.registerEndpoint({ method: 'PUT', uri: '/:pollId/comments/:commentId', handlers: [this.modifyCommentPollHandler], description: 'Modify a comment' });
+        this.registerEndpoint({ method: 'PATCH', uri: '/:pollId/comments/:commentId', handlers: [this.updateCommentPollHandler], description: 'Update a comment' });
     }
 
     /**
@@ -58,7 +76,7 @@ export default class PollController extends Controller {
                 id: poll.id
             });
         } catch (err) {
-            this.logger.error(err, {type: 'endpoints'});
+            this.logger.error(err, { type: 'endpoints' });
             return res.status(500).json({ error: err.message });
         }
     }
@@ -205,6 +223,193 @@ export default class PollController extends Controller {
                 links: [{
                     rel: 'gets the poll',
                     href: `${req.protocol}://${req.hostname}/polls/${poll.id}`
+                }],
+                id: poll.id
+            });
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    /**
+     * Add a comment to a poll
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `POST`
+     * - URI : `/:id/comments`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async addCommentPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.id);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            poll.comments.push({
+                author: req.body.author,
+                content: req.body.content
+            });
+            await poll.save();
+            return res.status(201).json({
+                links: [{
+                    rel: 'gets the created comment',
+                    href: `${req.protocol}://${req.hostname}/polls/${poll.id}/comments/${poll.comments[poll.comments.length -1]._id}`
+                }],
+                id: poll.id
+            });
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    /**
+     * Delete a comment to a poll
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `DELETE`
+     * - URI : `/:pollId/comments/:commentId`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async deleteCommentPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.pollId);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            _.remove(poll.comments, comment => comment._id == req.params.commentId);
+            poll.markModified('comments');
+            await poll.save();
+            return res.status(204).json();
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    /**
+     * Get a comment
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `GET`
+     * - URI : `/:pollId/comments/:commentId`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async getCommentPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.pollId);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            const com = poll.comments.find(comment => comment._id == req.params.commentId);
+            if (!com) {
+                return res.status(404).json({ error: 'Comment not found' });
+            }
+            return res.status(201).json(com);
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    /**
+     * List all comments
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `GET`
+     * - URI : `/:id/comments`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async listCommentsPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.id);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            return res.status(201).json(poll.comments);
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    // TODO ENDPONT MODIFY COMMENT 
+    /**
+     * Modify a comment
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `PUT`
+     * - URI : `/:pollId/comments/:commentId`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async modifyCommentPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.pollId);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            const com = poll.comments.find(comment => comment._id == req.params.commentId);
+            if (!com) {
+                return res.status(404).json({ error: 'Comment not found' });
+            }
+            com.author = req.body.author;
+            com.content =  req.body.content;
+            await poll.save();
+            return res.status(200).json({
+                links: [{
+                    rel: 'gets the comments',
+                    href: `${req.protocol}://${req.hostname}/polls/${poll.id}/comments/${com._id}`
+                }],
+                id: poll.id
+            });
+        } catch (err) {
+            this.logger.error(err, { type: 'endpoints' });
+            return res.status(500).json({ error: err.message });
+        }
+    }
+
+    /**
+     * Update a comment
+     * 
+     * This method is a handler / endpoint : 
+     * - Method : `PATCH`
+     * - URI : `/:pollId/comments/:commentId`
+     * 
+     * @param req 
+     * @param res 
+     */
+    public async updateCommentPollHandler(req: Request, res: Response): Promise<any> {
+        try {
+            const poll = await this.container.db.polls.findById(req.params.pollId);
+            if (!poll) {
+                return res.status(404).json({ error: 'Poll not found' });
+            }
+            const com = poll.comments.find(comment => comment._id == req.params.commentId);
+            if (!com) {
+                return res.status(404).json({ error: 'Comment not found' });
+            }
+            if(req.body.author) {
+                com.author = req.body.author;
+            }
+            if(req.body.content) {
+                com.content =  req.body.content;
+            }
+            await poll.save();
+            return res.status(200).json({
+                links: [{
+                    rel: 'gets the comments',
+                    href: `${req.protocol}://${req.hostname}/polls/${poll.id}/comments/${com._id}`
                 }],
                 id: poll.id
             });
